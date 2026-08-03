da3626e3e1f88cf62ee20ddf1b1f48ae5eb08558
AzzHakka <118124577+AzzHakka@users.noreply.github.com>
2026-08-03 21:00:33 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,32 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+  getDatabase,
+  ref,
+  set
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+const firebaseConfig = {
+  apiKey: "TON_API_KEY",
+  authDomain: "TON_PROJET.firebaseapp.com",
+  databaseURL: "https://TON_PROJET-default-rtdb.firebaseio.com",
+  projectId: "TON_PROJET"
+};
+
+
+// Connexion Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+// Fonction pour envoyer une position
+export function sendPosition(lat,lng){
+
+  set(ref(db,"position"),{
+    lat: lat,
+    lng: lng,
+    time: Date.now()
+  });
+
+}
